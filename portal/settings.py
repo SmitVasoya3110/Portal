29c29bb130a4d916f9dabf917d096a19c9f7e3fc
SmitVasoya3110 <68841925+SmitVasoya3110@users.noreply.github.com>
2024-03-14 23:02:19 +0530

    New theme

--- portal/settings.py
+++ portal/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
